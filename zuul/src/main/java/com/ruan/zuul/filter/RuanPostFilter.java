b268d94946996b337d4e74556c2a82815784c4e9
XY <919372671@qq.com>
2020-03-19 10:20:42 +0800

    网关添加请求前后拦截器

---
+++ zuul/src/main/java/com/ruan/zuul/filter/RuanPostFilter.java
@@ -0,0 +1,55 @@
+package com.ruan.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import static org.springframework.util.ReflectionUtils.rethrowRuntimeException;
+
+@Slf4j
+@Component
+public class RuanPostFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.SEND_RESPONSE_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        try {
+            RequestContext requestContext = RequestContext.getCurrentContext();
+            long startTime = (long) requestContext.get("startTime");
+            long endTime = System.currentTimeMillis();
+            long cost = endTime - startTime;
+            InputStream responseDataStream = requestContext.getResponseDataStream();
+            String body = StreamUtils.copyToString(responseDataStream, Charset.forName("UTF-8"));
+            if (body == null || "".equals(body)) {
+                body = requestContext.getResponseBody();
+            }
+            log.info("耗时：{}ms，response：{}", cost, body);
+        } catch (IOException e) {
+            e.printStackTrace();
+            rethrowRuntimeException(e);
+        }
+        return null;
+    }
+}
---
+++ zuul/src/main/java/com/ruan/zuul/filter/RuanPreFilter.java
@@ -0,0 +1,35 @@
+package com.ruan.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class RuanPreFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.FORM_BODY_WRAPPER_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        requestContext.set("startTime",System.currentTimeMillis());
+        log.info("接受到请求：{}", requestContext.getRequest().getRequestURL().toString());
+        return null;
+    }
+}
